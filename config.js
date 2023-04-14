6da63638277ac2c0989f640f51999f4687da1dbf
neelehoven <114346574+neelehoven@users.noreply.github.com>
2023-04-14 21:12:55 +0200

    Update config.js

--- config.js
+++ config.js
@@ -58,16 +58,16 @@ global.mess = {
   jobdone: "Job done...",
   useradmin: "Sorry, only *Group Admins* can use this command *Baka*!",
   botadmin:
-    "Sorry, i cant execute this command without being an *Admin* of this group.",
-  botowner: "Only my *Owner* can use this command, Baka!",
-  grouponly: "This command is only made for *Groups*, Baka!",
-  privateonly: "This command is only made for *Private Chat*, Baka!",
+    "Tut mir leid, ich kann diesen Befehl nicht ausführen, ohne ein *Admin* zu sein.",
+  botowner: "Sorry nur der *Owner* Kann diesen Command benutzen!",
+  grouponly: "Dieser Befehl ist nur für *Gruppe* Gemacht!",
+  privateonly: "Dieser Befehl ist nur für *Private Chat* Nutzbar!",
   botonly: "Only the *Bot itself* can use this command!",
-  waiting: "Chotto Matte...",
+  waiting: "Bitte Warten sie einen kurzen Moment...",
   nolink: "Please provide me *link*, Baka!",
   error: "An error occurd!",
   banned: `You are *Banned* fron using commands!  \n\nType *${prefa}owner* or *${prefa}support* to submit a request to unban yourself !`,
   bangc: "This Group is *Banned* from using Commands!",
-  nonsfw: "Dont be a pervert Baka! This is not a NSFW enabled group!",
+  nonsfw: "Sei nicht so Versaut du Schlingel der NSFW ist nicht Aktiv in der Gruppe!",
 };
 
